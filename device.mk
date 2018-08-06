32f50dcc6f2f5655b3e2c9113d2d3f9a7317689e
sbwml <984419930@qq.com>
2018-08-06 20:47:22 +0800

    sagit: dot-ify

--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Inherit from msm8998-common
 $(call inherit-product, device/xiaomi/msm8998-common/msm8998.mk)
@@ -45,6 +44,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
+
+# Remove_packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
+# LCD density
+TARGET_SYSTEM_PROP += \
+    $(DEVICE_PATH)/system.prop
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sagit/sagit-vendor.mk)
--- dot.mk
+++ dot.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := lineage_sagit
+PRODUCT_NAME := dot_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
